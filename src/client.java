ffbf8cbb1d86c7474afe1bf6262a97a86feb4b3f
7 <g>
2020-08-02 21:08:15 +0300

    fhhj

--- src/client.java
+++ src/client.java
@@ -1,3 +1,5 @@
+import org.apache.commons.codec.binary.Hex;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.DatagramPacket;
@@ -14,18 +16,25 @@ public class client {
            //         new BufferedReader(new InputStreamReader(System.in));
             DatagramSocket clientSocket = new DatagramSocket();
             InetAddress IPAddress = InetAddress.getByName("localhost");
-            byte[] sendData = new byte[1024];
-            byte[] receiveData = new byte[1024];
-           // String sentence = inFromUser.readLine();
-           // sendData = sentence.getBytes();
+            byte[] sendData = new byte[1154];
+            byte[] receiveData = new byte[1154];
+            String sentence="3FFF";
+            byte[] sendDataFirst = Hex.decodeHex(sentence);
+            System.out.println(sendDataFirst.length);
+            for (int i = 0; i < 2; i++) {
+                sendData[i]= sendDataFirst[i];
+            }
+
             final Random random = new Random();
-            for (int i = 0; i <1000 ; i++) {
+            for (int i = 2; i <1154 ; i++) {
                 sendData[i] =(byte) (random.nextInt(100));
             }
 
            // sendData = sentence.getBytes();
-            Thread.sleep(10000);
-
+            Thread.sleep(1000);
+            for (int i = 0; i < 2; i++) {
+                System.out.println((i+1)+":"+(int)sendData[i]);
+            }
 
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 55555);
             clientSocket.send(sendPacket);
